613ccf05fd2ef1326c721a3be683df46794ffe2e
Javier G. Sogo <jgsogo@gmail.com>
2018-12-12 21:04:39 +0100

    package

--- conanfile.py
+++ conanfile.py
@@ -15,3 +15,11 @@ class LibConan(ConanFile):
 
     export_sources = "README.md"
 
+    def package(self):
+        self.copy("*.h", dst="include", src="hello")
+        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*", dst="all")
